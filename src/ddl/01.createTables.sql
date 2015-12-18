b769f7c7d26fde73a3a0fcb8657c8700bf61f4d1
Rubén <ruboweb@yahoo.es>
2015-12-18 11:58:00 +0100

    User integration

--- src/ddl/01.createTables.sql
+++ src/ddl/01.createTables.sql
@@ -1,9 +1,22 @@
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
 CREATE TABLE `notification` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `creationDate` datetime NOT NULL,
-  `scheduleDate` datetime NOT NULL,
+  `user_id` bigint(20) DEFAULT NULL,  
+  `scheduleDate` date NOT NULL,
   `text` varchar(300) NOT NULL,
   `status` varchar(255) DEFAULT NULL,
   `trySend` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `FK_1urdwwsh2ti15ta6f6p5dbdcp` (`user_id`),
+  CONSTRAINT `FK_1urdwwsh2ti15ta6f6p5dbdcp` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+
+
--- src/ddl/02.data.sql
+++ src/ddl/02.data.sql
@@ -1,3 +1,5 @@
-insert into notification (creationDate, scheduleDate, text, status, trySend) values (curdate(), curdate(), "Sample text - Notification pending to send", null, null);
-insert into notification (creationDate, scheduleDate, text, status, trySend) values (curdate(), curdate(), "Sample text - Notification sent width error", "ERROR: xxxxxx", curdate());
-insert into notification (creationDate, scheduleDate, text, status, trySend) values (curdate(), curdate(), "Sample text - Notification pending to send", "SENT", curdate());
+insert into user (id, name, token) values (999, 'Dummy', '1234567890qwertyuiopasdfghjklzxcvbnm');
+
+insert into notification (user_id, creationDate, scheduleDate, text, status, trySend) values (999, curdate(), curdate(), "Sample text - Notification pending to send", null, null);
+insert into notification (user_id, creationDate, scheduleDate, text, status, trySend) values (999, curdate(), curdate(), "Sample text - Notification sent width error", "ERROR: xxxxxx", curdate());
+insert into notification (user_id, creationDate, scheduleDate, text, status, trySend) values (999, curdate(), curdate(), "Sample text - Notification pending to send", "SENT", curdate());
